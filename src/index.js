05ccb2cd679ce5f340f7950816ff6dac7ab671b4
nandinibansall <149954212+nandinibansall@users.noreply.github.com>
2025-09-23 23:43:47 +0530

    api and error handling

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ dotenv.config({
     path: './env'
 })
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port : ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>{
+    console.log("DB CONNECTION ERROR",err);
+})
 
 
 
