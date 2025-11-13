a5ab6270a7069c1df0a00534b9dfd35f8fc441ba
nandinibansall <149954212+nandinibansall@users.noreply.github.com>
2025-11-13 19:57:19 +0530

    fix: update response format in getCurrentUser to use apiResponse

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -227,7 +227,7 @@ const changeCurrentPassword= asyncHandler(async(req,res)=>{
 })
 
 const getCurrentUser= asyncHandler(async(req,res)=>{
-    return res.status(200).json(200,req.user,"current User Fected succesfully")
+    return res.status(200).json(new apiResponse(200,req.user,"current User Fected succesfully"))
 })
 
 const updateAccountDetails= asyncHandler(async(req,res)=>{
